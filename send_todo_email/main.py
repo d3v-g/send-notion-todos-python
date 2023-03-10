b229f3681b35396d34ed8c93df47df4d825c2e25
d3v-g <janewang@wangjiayes-MacBook-Air.local>
2023-03-10 12:42:26 +0000

    feat: allowed dynamic greeting depending on time of the day

--- send_todo_email/main.py
+++ send_todo_email/main.py
@@ -5,6 +5,7 @@ import os
 from string import Template
 from pathlib import Path
 from api import get_daily_tasks
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,6 +18,15 @@ def format_tasks(tasks):
         formatted_tasks += formatted_task
     return formatted_tasks
 
+def get_time_of_day():
+    hour = datetime.now().time().hour
+    if 12 <= hour <= 18:
+        return 'afternoon'
+    elif 19 <= hour <= 24:
+        return 'evening'
+    else:
+        return 'morning'
+
 
 def compile_email():
     email = EmailMessage()
@@ -24,12 +34,13 @@ def compile_email():
     email['to'] = os.getenv('TO_EMAIL')
     email['subject'] = 'Rise and shine \N{sun with face}, time for action!'
     
+    time_of_day = get_time_of_day()
     todos = format_tasks(get_daily_tasks('To Do'))
     doings = format_tasks(get_daily_tasks('Doing'))
     dones = format_tasks(get_daily_tasks('Done'))
 
     html = Template(Path('./send_todo_email/index.html').read_text())
-    content = html.substitute(todos=todos, doings=doings, dones=dones)
+    content = html.substitute(time_of_day=time_of_day, todos=todos, doings=doings, dones=dones)
     email.set_content(content, 'html')
 
     return email
